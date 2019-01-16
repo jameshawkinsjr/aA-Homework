288237a89803c103084070458fcd8fed306c5a5b
James <james@jameshawkins.com>
2019-01-15 20:51:06 -0800

    Add RSpec Homework

---
+++ W2D3/RSpec/spec/dessert_spec.rb
@@ -0,0 +1,67 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef") }
+  subject(:dessert) {Dessert.new("Brownie", 10, chef)}
+
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(dessert.type).to eq("Brownie")
+    end
+
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(10)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to eq([])
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect{Dessert.new("Test", "A", chef)}.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      expect(dessert.add_ingredient("Sugar")).to eq(["Sugar"])
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      expect(dessert.ingredients).to receive(:shuffle!)
+      dessert.mix!
+    end 
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+      expect(dessert.eat(9)).to eq(1)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect{dessert.eat(20)}.to raise_error(RuntimeError)
+    end
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef")
+      expect(dessert.serve).to eq("Chef has made 10 Brownies!")
+    end
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(dessert)
+      dessert.make_more
+    end
+  end
+end
