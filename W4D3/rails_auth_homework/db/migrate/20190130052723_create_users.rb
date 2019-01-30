1b81eede526ae2b6a5246a7c282d367c9ec6b1fe
James <james@jameshawkins.com>
2019-01-29 22:02:13 -0800

    Add Rails Auth Homework

--- W4D3/rails_auth_homework/db/migrate/20190130052723_create_users.rb
+++ W4D3/rails_auth_homework/db/migrate/20190130052723_create_users.rb
@@ -1,13 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :string
-      t.string :password_digest
-      t.string :string
-      t.string :session_token
-      t.string :string
-
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
       t.timestamps
     end
   end
